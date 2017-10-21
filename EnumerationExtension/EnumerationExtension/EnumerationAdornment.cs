d87fcb5684bafc111411635492ef57cad25cf169
Lando Tanghe <lando.tanghe@gmail.com>
2017-10-21 09:27:51 +0200

    refactor sample project

---
+++ EnumerationExtension/EnumerationExtension/EnumerationAdornment.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+
+namespace EnumerationExtension
+{
+    /// <summary>
+    /// TextAdornment1 places red boxes behind all the "a"s in the editor window
+    /// </summary>
+    internal sealed class EnumerationAdornment
+    {
+        public const string Name = "EnumerationAdornment";
+
+        /// <summary>
+        /// The layer of the adornment.
+        /// </summary>
+        private readonly IAdornmentLayer _layer;
+
+        private readonly TextHighlighter _textMarker;
+
+        /// <summary>
+        /// Text view where the adornment is created.
+        /// </summary>
+        private readonly IWpfTextView _view;
+
+        
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumerationAdornment"/> class.
+        /// </summary>
+        /// <param name="view">Text view to create the adornment for</param>
+        public EnumerationAdornment(IWpfTextView view)
+        {
+            if (view == null)
+            {
+                throw new ArgumentNullException("view");
+            }
+
+            _layer = view.GetAdornmentLayer(EnumerationAdornment.Name);
+
+            _view = view;
+            _view.LayoutChanged += OnLayoutChanged;
+
+            _textMarker = new TextHighlighter();
+        }
+
+        /// <summary>
+        /// Handles whenever the text displayed in the view changes by adding the adornment to any reformatted lines
+        /// </summary>
+        /// <remarks><para>This event is raised whenever the rendered text displayed in the <see cref="ITextView"/> changes.</para>
+        /// <para>It is raised whenever the view does a layout (which happens when DisplayTextLineContainingBufferPosition is called or in response to text or classification changes).</para>
+        /// <para>It is also raised whenever the view scrolls horizontally or when its size changes.</para>
+        /// </remarks>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        internal void OnLayoutChanged(object sender, TextViewLayoutChangedEventArgs e)
+        {
+            foreach (ITextViewLine line in e.NewOrReformattedLines)
+            {
+                this.CreateVisuals(line);
+            }
+        }
+
+        /// <summary>
+        /// Adds the scarlet box behind the 'a' characters within the given line
+        /// </summary>
+        /// <param name="line">Line to add the adornments</param>
+        private void CreateVisuals(ITextViewLine line)
+        {
+            IWpfTextViewLineCollection textViewLines = _view.TextViewLines;
+
+            // Loop through each character, and place a box around any 'a'
+            for (int charIndex = line.Start; charIndex < line.End; charIndex++)
+            {
+                if (_view.TextSnapshot[charIndex] == 'a')
+                {
+                    SnapshotSpan span = new SnapshotSpan(_view.TextSnapshot, Span.FromBounds(charIndex, charIndex + 1));
+                    Geometry geometry = textViewLines.GetMarkerGeometry(span);
+                    if (geometry != null)
+                    {
+                        Image image = _textMarker.Highlight(geometry);
+                        _layer.AddAdornment(AdornmentPositioningBehavior.TextRelative, span, null, image, null);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ EnumerationExtension/EnumerationExtension/TextHighlighter.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace EnumerationExtension
+{
+    public class TextHighlighter
+    {
+        /// <summary>
+        /// Adornment brush.
+        /// </summary>
+        private readonly Brush _brush;
+
+        /// <summary>
+        /// Adornment pen.
+        /// </summary>
+        private readonly Pen _pen;
+
+        public TextHighlighter()
+        {
+            // Create the pen and brush to color the box behind the a's
+            _brush = new SolidColorBrush(Color.FromArgb(0x20, 0x00, 0x00, 0xee));
+            _brush.Freeze();
+
+            var _penBrush = new SolidColorBrush(Colors.Red);
+            _penBrush.Freeze();
+            _pen = new Pen(_penBrush, 0.5);
+            _pen.Freeze();
+        }
+
+        public Image Highlight(Geometry geometry)
+        {
+            var drawing = new GeometryDrawing(_brush, _pen, geometry);
+            drawing.Freeze();
+
+            var drawingImage = new DrawingImage(drawing);
+            drawingImage.Freeze();
+
+            var image = new Image
+            {
+                Source = drawingImage,
+            };
+
+            // Align the image with the top of the bounds of the text geometry
+            Canvas.SetLeft(image, geometry.Bounds.Left);
+            Canvas.SetTop(image, geometry.Bounds.Top);
+            return image;
+        }
+    }
+}
